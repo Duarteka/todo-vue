dae6ccedbec56985764fc72a0143840eb57dea3f
Duarteka <duarte.karen21@gmail.com>
2021-03-09 13:25:40 +0100

    complex system finished

--- js/funciones.js
+++ js/funciones.js
@@ -1,15 +1,37 @@
 Vue.component("tarea",{
     data : function(){
         return {
-            editando : false
+            
+            tareaEditada : this.tarea
         }
     },
-    props : ["id","tarea","terminada"],
+    props : ["id","tarea","terminada","editando"],
+    methods : {
+        editarTarea(){
+            if(this.editando){
+                //guardar
+                if(this.tareaEditando != this.tarea){
+                    this.$emit("guardar", this.id, this.tareaEditada);
+                }else{
+                    this.editando = false;
+                }
+            }else{
+                //editar
+                this.editando = true
+            }
+        }
+    },
+//boton cambiar tarea que se cambie a editando
+//que se guarde tarea al guardar
     template : `<div class="tarea"> 
-                    <h3 :class="[ !editando ? 'visible' : null ]">{{tarea}}</h3>
-                    <input type="text" :class="[ editando ? 'visible' : null ]" v-model="tarea">
-                    <a href="#" class="boton" @click.prevent="editando = !editando">{{ editando ? 'guardar' : 'editar' }}</a>
-                    <a href="#" class="boton">borrar</a>
+                    <h3 :class="[ !editando ? 'visible' : null ]">{{tarea}}</h3> 
+                    
+                    <input type="text" :class="[ editando ? 'visible' : null ]" v-model="tareaEditada">
+                    
+                    <a href="#" class="boton" @click.prevent="editarTarea">{{ editando ? 'guardar' : 'editar' }}</a>
+                    
+                    <a href="#" class="boton" @click.prevent="$emit('borrar', id)">borrar</a>
+                    
                     <a href="#" :class="['estado', terminada ? 'terminada' : null ]"
                     @click.prevent="$emit('toggle',id)"><span></span></a>
                 </div> `
@@ -21,6 +43,35 @@ var todo = new Vue({
         tareaTemporal : "",
     },
     methods : {
+
+        crearTarea(tarea){
+            console.log(tarea)
+            ajax('POST', { tarea : tarea })
+            .then(datos =>{
+                if(datos.resultado == "ok"){
+                    this.tareas.push({
+                        id : datos.id, 
+                        tarea : tarea,
+                        terminada : false 
+                    });
+                    this.tareaTemporal = "";
+                }
+            });
+        },
+        guardarTarea(id,tarea){
+            ajax("PUT", { id : id, tarea : tarea, operacion : 1 })
+            .then(datos => {
+                if(datos.resultado == "ok"){
+                    
+                    //console.log("editado")
+                    this.tareas = this.tareas.map(item => {
+                        item.id  == id ? item.tarea = tarea : null;
+                        return item;
+                    })
+                }
+            });
+        },
+
         toggleEstado(id){
             ajax('PUT',{ id : id, operacion : 2})
             .then(datos =>{
@@ -29,10 +80,17 @@ var todo = new Vue({
                        item.terminada = item.id == id ? !item.terminada : item.terminada
                        return item;
                     });
-
                 }
             });
             //console.log(id)
+        },
+        borrarTarea(id){
+            ajax('DELETE', { id : id })
+            .then(datos =>{
+                if(datos.resultado === "ok"){
+                    this.tareas = this.tareas.filter(item => item.id != id)
+                }
+            });
         }
     },
     mounted(){
